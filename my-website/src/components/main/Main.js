9b7025900212656009189f7247705861a5e13a77
vivian <vivian.v18723@gmail.com>
2022-01-17 16:18:46 +0000

    Import Projects.js file in Main.js file.

--- my-website/src/components/main/Main.js
+++ my-website/src/components/main/Main.js
@@ -1,9 +1,11 @@
 import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
 
 const Main = () => {
   return (
     <main>
       <AboutMe />
+      <Projects />
     </main>
   );
 };
