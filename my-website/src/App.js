d1033b614cb87a142852a7270d2d76ce1413b5d2
vivian <vivian.v18723@gmail.com>
2022-01-17 16:07:17 +0000

    Import all 3 components in App.js file.

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
+import "./App.css";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
 
 function App() {
   return (
     <div>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
