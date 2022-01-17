de55e8bef5926e51b591267ef91f0be013d853f2
vivian <vivian.v18723@gmail.com>
2022-01-17 16:09:03 +0000

    Add JSX to Header component.

--- my-website/src/components/header/Header.js
+++ my-website/src/components/header/Header.js
@@ -2,7 +2,23 @@ const Header = () => {
   return (
     <header>
       <nav>
-        <p>This is where the header goes</p>
+        <ul className="all-nav-items">
+          <li className="nav-items">
+            <a href="#home" className="nav-link">
+              Home
+            </a>
+          </li>
+          <li className="nav-items">
+            <a href="#projects" className="nav-link">
+              Projects
+            </a>
+          </li>
+          <li className="nav-items">
+            <a href="#skills" className="nav-link">
+              Skills
+            </a>
+          </li>
+        </ul>
       </nav>
     </header>
   );
