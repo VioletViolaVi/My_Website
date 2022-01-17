d706c7c68255475a668a563aab5501d492caad69
vivian <vivian.v18723@gmail.com>
2022-01-17 17:45:09 +0000

    Pass custom props through Project function

--- my-website/src/components/main/components/Projects.js
+++ my-website/src/components/main/components/Projects.js
@@ -1,25 +1,18 @@
-import busybakesImg from "./project-images/busybakes.png";
-
-const Projects = () => {
+const Projects = (props) => {
   return (
     <section>
       <figure>
-        <a
-          href="https://violetviolavi.github.io/Milestone-Project-1/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={busybakesImg} alt="BusyBakes website link" />
-          <figcaption>BusyBakes</figcaption>
+        <a href={props.customLink} target="_blank" rel="noreferrer">
+          <img
+            src={"./project-images/" + props.customImg}
+            alt={props.customAlt}
+          />
         </a>
+        <figcaption>{props.customTitle}</figcaption>
       </figure>
+      <p>{props.customDescription}</p>
       <p>
-        A baking club website for users to browse and select baking lessons they
-        wish to take.
-      </p>
-      <p>
-        <b>Technologies:</b> HTML5, CSS3, SASS, jQuery, JavaScript, Bootstrap,
-        Git, Gitpod, Github, Balsamiq Wireframes
+        <b>Technologies:</b> {props.customTechnologies}
       </p>
     </section>
   );
