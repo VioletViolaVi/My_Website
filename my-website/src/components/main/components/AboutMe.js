39dda808ee8bc5cfd22c1c330c834d7c48d8853d
vivian <vivian.v18723@gmail.com>
2022-01-17 16:27:00 +0000

    Add info and image using JSX to AboutMe.js.

--- my-website/src/components/main/components/AboutMe.js
+++ my-website/src/components/main/components/AboutMe.js
@@ -1,7 +1,35 @@
+import portrait from "./images/avatar.png";
+
 const AboutMe = () => {
   return (
     <section>
-      <p>This is where the about me goes</p>
+      <header>
+        <h1>Front End Developer</h1>
+      </header>
+
+      <p>
+        Hello!{" "}
+        <span>
+          <i className="fas fa-hand-sparkles"></i>
+        </span>
+      </p>
+
+      <p>I'm a Front End Developer.</p>
+
+      <p>
+        I write clean, maintainable and reusable code to create beautiful user
+        interfaces.
+      </p>
+
+      <figure>
+        <img src={portrait} alt="Cartoon version of me." />
+        <figcaption>
+          Hello Again!{" "}
+          <span>
+            <i className="fas fa-hand-sparkles"></i>
+          </span>
+        </figcaption>
+      </figure>
     </section>
   );
 };
