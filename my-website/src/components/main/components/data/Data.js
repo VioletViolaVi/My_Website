e2e3b41d5f8d142a1073498d1cafce8759e70348
vivian <vivian.v18723@gmail.com>
2022-01-17 16:55:16 +0000

    Add the data of projects to Data.js.

---
+++ my-website/src/components/main/components/data/Data.js
@@ -0,0 +1,50 @@
+const Data = [
+  {
+    id: 1,
+    title: "BusyBakes",
+    link: "https://violetviolavi.github.io/Milestone-Project-1/",
+    image: "busybakes.png",
+    alt: "BusyBakes website link",
+    description:
+      "A baking club website for users to browse and select baking lessons they wish to take.",
+    technologies:
+      "HTML5, CSS3, SASS, jQuery, JavaScript, Bootstrap, Git, Gitpod, Github, Balsamiq Wireframes",
+  },
+
+  {
+    id: 2,
+    title: "Sadie's Zoo",
+    link: "https://violetviolavi.github.io/Milestone-Project-2/",
+    image: "sadieszoo.png",
+    alt: "Sadie's Zoo website link",
+    description:
+      "A memory card matching game for users to play to pass time by.",
+    technologies:
+      "HTML5, CSS3, SASS, jQuery, JavaScript, Git, Gitpod, Github, Balsamiq Wireframes",
+  },
+
+  {
+    id: 3,
+    title: "Cinemagic",
+    link: "https://milestone-project-3-vivian.herokuapp.com/",
+    image: "cinemagic.png",
+    alt: "Cinemagic website link",
+    description:
+      "A movie ticket booking website for users to browse and select tickets for movies wanted.",
+    technologies:
+      "HTML5, CSS3, jQuery, JavaScript, Materialize, Git, Gitpod, Github, Balsamiq Wireframes",
+  },
+
+  {
+    id: 4,
+    title: "Slurps",
+    link: "https://milestone-project-4-vivian.herokuapp.com/",
+    image: "slurps.png",
+    alt: "Slurps website link",
+    description:
+      "An e-commerce website for users to buy milkshakes and juices online.",
+    technologies:
+      "HTML5, CSS3, jQuery, JavaScript, Bootstrap, Git, Gitpod, Github, Balsamiq Wireframes",
+  },
+];
+export default Data;
