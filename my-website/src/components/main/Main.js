937caed38e16e776935296e8642aba445550cbe6
vivian <vivian.v18723@gmail.com>
2022-01-17 16:19:36 +0000

    Import Skills.js file in Main.js file.

--- my-website/src/components/main/Main.js
+++ my-website/src/components/main/Main.js
@@ -1,11 +1,13 @@
 import AboutMe from "./components/AboutMe";
 import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 const Main = () => {
   return (
     <main>
       <AboutMe />
       <Projects />
+      <Skills />
     </main>
   );
 };
