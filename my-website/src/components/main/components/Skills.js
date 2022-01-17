c7d26dabbfa2f3cfcd1a3797ff11fd4630b7555d
vivian <vivian.v18723@gmail.com>
2022-01-17 18:07:24 +0000

    Add info and image to Skills.js

--- my-website/src/components/main/components/Skills.js
+++ my-website/src/components/main/components/Skills.js
@@ -1,7 +1,18 @@
+import diploma from "./images/diploma.png";
+
 const Skills = () => {
   return (
     <section>
-      <p>This is where the skills goes</p>
+      <ul>
+        <li>HTML5 | CSS3 | SASS</li>
+        <li>JavaScript | jQuery | React | Bootstrap</li>
+        <li>Materialize | Balsamiq Wireframes | Git</li>
+        <li>VS Code | Gitpod | GitHub</li>
+      </ul>
+      <figure>
+        <img src={diploma} alt="Software Development Diploma." />
+        <figcaption>My Software Development Diploma</figcaption>
+      </figure>
     </section>
   );
 };
