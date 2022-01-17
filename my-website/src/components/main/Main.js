99b79fe167d26ceb1f9e8ffa52dcb8717e7e510c
vivian <vivian.v18723@gmail.com>
2022-01-17 17:14:37 +0000

    Pass props and map through Projects component.

--- my-website/src/components/main/Main.js
+++ my-website/src/components/main/Main.js
@@ -1,12 +1,26 @@
 import AboutMe from "./components/AboutMe";
+import Data from "./components/data/Data";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
 const Main = () => {
+  const allProjects = Data.map((singleDataObj) => {
+    return (
+      <Projects
+        customId={singleDataObj.id}
+        customTitle={singleDataObj.title}
+        customLink={singleDataObj.link}
+        customImg={singleDataObj.image}
+        customAlt={singleDataObj.alt}
+        customDescription={singleDataObj.description}
+        customTechnologies={singleDataObj.technologies}
+      />
+    );
+  });
   return (
     <main>
       <AboutMe />
-      <Projects />
+      {allProjects}
       <Skills />
     </main>
   );
