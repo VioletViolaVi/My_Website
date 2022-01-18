78b9e1652f90f977e99e89db530c7ada1ed80742
vivian <vivian.v18723@gmail.com>
2022-01-18 19:46:07 +0000

    Add button to scroll back up to the top.

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import ScrollUpBtn from "./components/back-to-top-btn/ScrollUpBtn";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
@@ -6,6 +7,7 @@ import Main from "./components/main/Main";
 function App() {
   return (
     <>
+      <ScrollUpBtn />
       <Header />
       <Main />
       <Footer />
