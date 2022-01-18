e41681f756751382bb3a7162aeb6ad0d86748ec4
vivian <vivian.v18723@gmail.com>
2022-01-18 18:46:59 +0000

    Add links to icons in Footer.js

--- my-website/src/components/footer/Footer.js
+++ my-website/src/components/footer/Footer.js
@@ -1,8 +1,23 @@
 const Footer = () => {
   return (
     <footer className="footer-bg">
-      <i className="fas fa-envelope"></i>
-      <i className="fab fa-github"></i>
+      <a
+        href="mailto:vivi39261@gmail.com"
+        target="_blank"
+        rel="noreferrer"
+        className="footer-link"
+      >
+        <i className="fas fa-envelope"></i>
+      </a>
+
+      <a
+        href="https://github.com/VioletViolaVi?tab=repositories"
+        target="_blank"
+        rel="noreferrer"
+        className="footer-link"
+      >
+        <i className="fab fa-github"></i>
+      </a>
     </footer>
   );
 };
